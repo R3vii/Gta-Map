d30c1e9ba166ce1e11293c7ff185d024fa563388
R3vii <revi.pingwin@gmail.com>
2024-10-31 16:49:40 +0100

    Add files via upload

---
+++ markersData.js
@@ -0,0 +1,32 @@
+// markersData.js
+var markersData = [
+    { id: 'Mumia_1', lat: 2106.74, lng: 3169.58, title: 'Mumia_1', imageUrl: 'Mumia1.png', countAs: 1 },
+    { id: 'Mumia_2', lat: 2165.49, lng: 3641.83, title: 'Mumia_2', imageUrl: 'Mumia2.png', countAs: 1 },
+    { id: 'Mumia_3', lat: 2270.75, lng: 3990.00, title: 'Mumia_3', imageUrl: 'Mumia3.png', countAs: 1},
+    { id: 'Mumia_4', lat: 2260.88, lng: 3939.00, title: 'Mumia_4', imageUrl: 'Mumia4.png', countAs: 1},
+    { id: 'Mumia_5', lat: 2091.38, lng: 4501.75, title: 'Mumia_5', imageUrl: 'Mumia5.png', countAs: 1 },
+    { id: 'Mumia_6', lat: 2190.88, lng: 4529.00, title: 'Mumia_6', imageUrl: 'Mumia5_6.png', countAs: 1 },
+    { id: 'Mumia_7', lat: 2311.13, lng: 4483.38, title: 'Mumia_7', imageUrl: 'Mumia6.png', countAs: 1 },
+    { id: 'Mumia_8', lat: 2536.00, lng: 4594.88, title: 'Mumia_8', imageUrl: 'Mumia7.png', countAs: 1 },
+    { id: 'Mumia_9', lat: 2591.63, lng: 4702.13, title: 'Mumia_9', imageUrl: 'Mumia8.png', countAs: 1 },
+    { id: 'Mumia_10', lat: 2819.75, lng: 4470.63, title: 'Mumia_10', imageUrl: 'Mumia9.png', countAs: 1 },
+    { id: 'Mumia_11', lat: 2722.63, lng: 4481.13, title: 'Mumia_11', imageUrl: 'Mumia10.png', countAs: 1 },
+    { id: 'Mumia_12', lat: 3471.50, lng: 4890.00, title: 'Mumia_12', imageUrl: 'Mumia11.png', countAs: 1 },
+    { id: 'Mumia_13', lat: 2845.63, lng: 3502.88, title: 'Mumia_13', imageUrl: 'Mumia12.png', countAs: 1 },
+    { id: 'Mumia_14', lat: 2656.00, lng: 3006.50, title: 'Mumia_14', imageUrl: 'Mumia13.png', countAs: 1 },
+    { id: 'Mumia_15', lat: 2593.25, lng: 3527.00, title: 'Mumia_15', imageUrl: 'Mumia14.png', countAs: 1 },
+    { id: 'Mumia_16_17', lat: 1926.00, lng: 2634.38, title: 'Mumia_16_17', imageUrl: 'Mumia15.png', countAs: 2 },
+    { id: 'Mumia_18', lat: 3509.50, lng: 5334.00, title: 'Mumia_18', imageUrl: 'Mumia18.png', countAs: 1 },
+    //Nowe
+    { id: 'Mumia_19_20', lat: 4014.63, lng: 5228.63, title: 'Mumia_19_20', imageUrl: 'Mumia19_20.png', countAs: 2 },
+    { id: 'Mumia_21', lat: 4383.00, lng: 5009.38, title: 'Mumia_21', imageUrl: 'Mumia21.png', countAs: 1 },
+    { id: 'Mumia_22', lat: 1804.50, lng: 3394.00, title: 'Mumia_22', imageUrl: 'Mumia22.png', countAs: 1 },
+    { id: 'Mumia_23', lat: 1875.75, lng: 3931.25, title: 'Mumia_23', imageUrl: 'Mumia23.png', countAs: 1 },
+    { id: 'Mumia_24', lat: 1661.00, lng: 3165.88, title: 'Mumia_24', imageUrl: 'Mumia24.png', countAs: 1 },
+    { id: 'Mumia_25', lat: 6067.75, lng: 5059.25, title: 'Mumia_25', imageUrl: 'Mumia25.png', countAs: 1 },
+    { id: 'Mumia_26', lat: 1927.38, lng: 2672.38, title: 'Mumia_26', imageUrl: 'Mumia26.png', countAs: 1 },
+    { id: 'Mumia_27', lat: 2824.38, lng: 4463.50, title: 'Mumia_27', imageUrl: 'Mumia27.png', countAs: 1 },
+    { id: 'Mumia_28', lat: 2775.25, lng: 4436.25, title: 'Mumia_28', imageUrl: 'Mumia28.png', countAs: 1 },
+    
+
+];
